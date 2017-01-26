8674bffb582efe395465d90a463e3a5c6522d583
Robert Kim <rkty13@gmail.com>
2017-01-25 23:21:30 -0500

    Refactored linear_system.py.

--- linear_system.py
+++ linear_system.py
@@ -4,21 +4,11 @@ from vector import Vector
 from exceptions import DimensionError
 from copy import deepcopy
 
-class LinearSystem(object):
-    def __init__(self, equations = 0, unknowns = 0, A = None, b = None, fractional = True):
-        self._fractional = fractional
-        if A and not isinstance(A, Matrix):
-            raise DimensionError("A must be of type Matrix.")
-        if b and not isinstance(b, Vector):
-            raise DimensionError("b must be of type Vector.")
-
-        if A and b and len(A) != len(b):
-            raise DimensionError("The number of linear equations in " + 
-                                    "A does not match the number of solutions in b.")
-        self._A = deepcopy(A) if A else \
-                    Matrix(rows = equations, cols = unknowns, fractional = self._fractional)
+class LinearSystem(Matrix):
+    def __init__(self, rows = 0, cols = 0, A = None, b = None, fractional = True):
+        super().__init__(rows = rows, cols = cols, lst = A, fractional = fractional)
         self._b = deepcopy(b) if b else \
-                    Vector(length = equations, fractional = self._fractional)
+                    Vector(length = rows, fractional = fractional)
 
     def rref(self):
         return self._gauss_jordan_elimination(reduced = True)
@@ -27,7 +17,7 @@ class LinearSystem(object):
         new_LS = deepcopy(self)
         i = 0
         while i < len(new_LS):
-            f = new_LS._A[i].leading_term_index()
+            f = self[i].leading_term_index()
             if f < 0:
                 i += 1
                 continue
@@ -37,19 +27,19 @@ class LinearSystem(object):
 
     def _row_op_mult(self, LS, row_i, k):
         new_LS = deepcopy(LS)
-        new_LS._A = new_LS._A._row_op_mult(new_LS._A, row_i, k)
+        self = self._row_op_mult(self, row_i, k)
         new_LS._b = new_LS._b._row_op_mult(new_LS._b, row_i, k)
         return new_LS
 
     def _row_op_swap(self, LS, row_i, row_j):
         new_LS = deepcopy(LS)
-        new_LS._A = new_LS._A._row_op_swap(new_LS._A, row_i, row_j)
+        self = self._row_op_swap(self, row_i, row_j)
         new_LS._b = new_LS._b._row_op_swap(new_LS._b, row_i, row_j)
         return new_LS
 
     def _row_op_add(self, LS, from_i, to_i, k):
         new_LS = deepcopy(LS)
-        new_LS._A = new_LS._A._row_op_add(new_LS._A, from_i, to_i, k)
+        self = self._row_op_add(self, from_i, to_i, k)
         new_LS._b = new_LS._b._row_op_add(new_LS._b, from_i, to_i, k)
         return new_LS
 
@@ -77,26 +67,22 @@ class LinearSystem(object):
         return B.simplify()
 
     def __len__(self):
-        return len(self._A)
+        return len(self._m)
 
     def __str__(self):
-        return str({"A": str(self._A), "b": str(self._b)})
+        return str({"A": str(self._m), "b": str(self._b)})
 
     def __repr__(self):
         return self.__str__()
 
     def __getitem__(self, x):
-        return Vector(lst = list(self._A[x]) + [self._b[x]])
+        return Vector(lst = list(self._m[x]) + [self._b[x]])
 
     def __copy__(self):
         LS = type(self)()
-        LS._A = self._A
+        LS._m = self._m
         LS._b = self._b
         return LS
 
     def __deepcopy__(self, memo):
-        LS = type(self)()
-        LS._A = deepcopy(self._A, memo)
-        LS._b = deepcopy(self._b, memo)
-        return LS
-        
+        return type(self)(rows = self._rows, cols = self._cols, A = self._m, b = self._b, fractional = self._fractional)      
