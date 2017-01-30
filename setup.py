76c5aa1164091b3936f5858f2736e56d83231a99
Robert Kim <rkty13@gmail.com>
2017-01-29 22:28:11 -0500

    Updated setup for pypi.

--- setup.py
+++ setup.py
@@ -8,12 +8,10 @@ exec(open("matrox/version.py", "r").read())
 setup(
     name="matrox",
     version=__version__,
-    install_requires=requirements,
     author="Robert Kim",
     author_email="me@robertkim.io",
     packages=["matrox", "matrox.linalg", "matrox.tests"],
     url="https://github.com/rkty13/matrox",
     license="The MIT License (MIT)",
-    description="Linear Algebray library.",
-    include_package_data=False
+    description="Linear Algebra library."
 )
