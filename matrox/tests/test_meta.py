2c2151c1b050f93b5b09fd294a182d765fd3d075
Robert Kim <rkty13@gmail.com>
2017-02-02 00:20:54 -0500

    Updated version number.

--- matrox/tests/test_meta.py
+++ matrox/tests/test_meta.py
@@ -5,4 +5,4 @@ from matrox.version import __version__
 class TestMeta(unittest.TestCase):
 
     def test_version(self):
-        self.assertEqual(__version__, "0.1.2")
+        self.assertEqual(__version__, "0.1.3")
