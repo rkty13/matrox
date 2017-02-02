ce558159c8fb5d740f710f36e4a484f3a9f72398
Robert Kim <rkty13@gmail.com>
2017-02-02 01:33:47 -0500

    Added inverse function.

--- matrox/linalg/properties.py
+++ matrox/linalg/properties.py
@@ -1,7 +1,27 @@
-from . import ref
+from . import (
+    gaussian_elimination,
+    ref
+)
 
-from matrox import num_non_zero_rows
+from matrox import (
+    assert_square_matrix,
+    identity_matrix,
+    is_identity,
+    num_non_zero_rows,
+    num_rows
+)
 
 def rank(matrix):
     reduced = ref(matrix)
     return num_non_zero_rows(reduced[0])
+
+@assert_square_matrix
+def inverse(matrix):
+    reduced, traceback, _ = gaussian_elimination(matrix, history=True)
+    inverted = identity_matrix(num_rows(matrix))
+    if is_identity(reduced):
+        for el in traceback:
+            inverted = el * inverted
+        return inverted
+    else:
+        return None
