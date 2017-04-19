40c9fbf5eee1ff1f1b8d4091a00ca3fde7700162
Robert Kim <rkty13@gmail.com>
2017-04-18 22:50:28 -0400

    Added tests for null_basis.

--- matrox/tests/linalg/test_spaces.py
+++ matrox/tests/linalg/test_spaces.py
@@ -5,6 +5,14 @@ from matrox.linalg import *
 
 class TestSpaces(unittest.TestCase):
 
+    def test_null_basis(self):
+        matrix_a = Matrix([[0, 2, 0, 0], 
+                            [3, -1, 0, 0],
+                            [0, 0, 0, 5], 
+                            [0, 0, 0, 0]], fraction=True)
+        self.assertEqual(repr(null_basis(matrix_a)),
+            "[Matrix([['0'], ['0'], ['1'], ['0']])]")
+ 
     def test_back_substitution(self):
         U = Matrix([[1, 1, 2, 3], [0, 0, 4, 4], [0, 0, 0, 0]], fraction=True)
         x = Matrix([[0], [1], [0], [0]], fraction=True)
